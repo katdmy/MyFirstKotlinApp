543dd3a4d90cd3b3bc0e5fcb7fdffa06c04fc32d
katdmy <dmykatkov@gmail.com>
2021-01-01 15:05:05 +1000

    Refactored app to MVVM pattern. MovieDetailsFragment and MovieListFragment shares ViewModel. MoviesModel will be refactored in the following homeworks to use a database and internet data fetching. Renamed MovivesModel to MoviesRepostrory Changed context from application to invoked Fragment in MoviesRepository Changed coroutines context in MoviesRepository from scope to withContext(dispacher), which is Dispatcher.IO Changed return result from LiveData<List<Movies>> to List<Movies> in MoviesRepository Changed inherited class from AndroidViewModel to ViewModel in MoviesViewModel Changed adapters onClickListener implementations from interface and its implementations to lambdas Refactored data classes to use @Parcelize annotation

--- app/src/main/java/com/katdmy/android/myfirstkotlinapp/data/Actor.kt
+++ app/src/main/java/com/katdmy/android/myfirstkotlinapp/data/Actor.kt
@@ -1,7 +1,11 @@
 package com.katdmy.android.myfirstkotlinapp.data
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Actor(
     val id: Int,
     val name: String,
     val picture: String
-)
+) : Parcelable
--- app/src/main/java/com/katdmy/android/myfirstkotlinapp/movieslist/MovieListFragment.kt
+++ app/src/main/java/com/katdmy/android/myfirstkotlinapp/movieslist/MovieListFragment.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.katdmy.android.myfirstkotlinapp.R
